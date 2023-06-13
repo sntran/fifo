d70644f60d64ba7c2f5e738d7766b0874de06c96
Son Tran-Nguyen <github@sntran.com>
2023-06-12 22:01:44 -0500

    feat: count with KV

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -1,8 +1,12 @@
-import { useState } from "preact/hooks";
+import { type StateUpdater, useState } from "preact/hooks";
 import { Button } from "../components/Button.tsx";
 
 interface CounterProps {
   start: number;
+}
+
+async function fetchSetCount(newCount: number) {
+  return await fetch(`/api/count?new_count=${newCount}`, { method: "POST" });
 }
 
 export default function Counter(props: CounterProps) {
@@ -10,8 +14,28 @@ export default function Counter(props: CounterProps) {
   return (
     <div class="flex gap-2 w-full">
       <p class="flex-grow-1 font-bold text-xl">{count}</p>
-      <Button onClick={() => setCount(count - 1)}>-1</Button>
-      <Button onClick={() => setCount(count + 1)}>+1</Button>
+      <Button
+        onClick={async () => {
+          const newCount = count - 1;
+          const resp = await fetchSetCount(newCount);
+          if (resp.ok) {
+            setCount(newCount);
+          }
+        }}
+      >
+        -1
+      </Button>
+      <Button
+        onClick={async () => {
+          const newCount = count + 1;
+          const resp = await fetchSetCount(newCount);
+          if (resp.ok) {
+            setCount(newCount);
+          }
+        }}
+      >
+        +1
+      </Button>
     </div>
   );
 }
