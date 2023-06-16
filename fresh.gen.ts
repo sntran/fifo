e0addc9e71b635f0fa2018c84e7a74b4fcaa98c6
Son Tran-Nguyen <github@sntran.com>
2023-06-15 21:20:31 -0500

    feat: upgrade to fresh 1.2

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,7 +2,6 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/index.tsx";
 import * as $$0 from "./islands/Navigator.tsx";
@@ -18,7 +17,6 @@ const manifest = {
     "./islands/Tree.tsx": $$1,
   },
   baseUrl: import.meta.url,
-  config,
 };
 
 export default manifest;
